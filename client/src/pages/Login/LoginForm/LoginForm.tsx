2bb4ce2621cd314b1e644357c17e4da9c1d85119
Alex <wonsangalex@gmail.com>
2021-07-30 17:15:27 -0700

    username field deleted and a div for height adjustment deleted

--- client/src/pages/Login/LoginForm/LoginForm.tsx
+++ client/src/pages/Login/LoginForm/LoginForm.tsx
@@ -91,7 +91,6 @@ export default function Login({ handleSubmit }: Props): JSX.Element {
                 {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Login'}
               </Button>
             </Box>
-            <div style={{ height: 95 }} />
           </form>
           <DemoButton />
         </>
