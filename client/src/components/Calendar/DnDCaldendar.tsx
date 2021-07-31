d276f115d0c869030ef7bc5789365e587ff2117d
Alex <wonsangalex@gmail.com>
2021-07-30 17:13:15 -0700

    color label added to a events

--- client/src/components/Calendar/DnDCaldendar.tsx
+++ client/src/components/Calendar/DnDCaldendar.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer, ToolbarProps, Event, EventProps } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import useStyles from './useStyles';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import moment from 'moment';
 import { IUserSchedule, mockDatas } from './MockEvent';
 import { useState } from 'react';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const localizer = momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(Calendar as any);
+
+class CalendarEvent {
+  title: string;
+  allDay: boolean;
+  start: Date;
+  endDate: Date;
+  desc: string;
+  resourceId?: string | undefined;
+  tooltip?: string | undefined;
+
+  constructor(_title: string, _start: Date, _endDate: Date, _allDay?: boolean, _desc?: string, _resourceId?: string) {
+    this.title = _title;
+    this.allDay = _allDay || false;
+    this.start = _start;
+    this.endDate = _endDate;
+    this.desc = _desc || '';
+    this.resourceId = _resourceId;
+  }
+}
+
+class CalendarResource {
+  title: string;
+  id: string;
+
+  constructor(id: string, title: string) {
+    this.id = id;
+    this.title = title;
+  }
+}
 
 const DnDCalendar = (): JSX.Element => {
   const [events, setEvent] = useState<IUserSchedule[]>(mockDatas);
 
   const moveEvent = ({ event, start, end }: { event: any; start: any; end: any }): void => {
-    const idx = events.indexOf(event);
+    const selectedIndex = events.indexOf(event);
     const updatedEvent = { ...event, start, end };
 
     const nextEvents = [...events];
-    nextEvents.splice(idx, 1, updatedEvent);
+    nextEvents.splice(selectedIndex, 1, updatedEvent);
 
     setEvent(nextEvents);
   };
@@ -27,12 +57,40 @@ const DnDCalendar = (): JSX.Element => {
       selectable
       localizer={localizer}
       events={events}
+      onDragStart={console.log}
       onEventDrop={moveEvent}
-      resizable
+      components={{
+        toolbar: Toolbar,
+        event: EventComponent,
+      }}
+      defaultView={'month'}
       defaultDate={new Date(2021, 7, 1)}
-      style={{ minHeight: '600px' }}
+      style={{ minHeight: '800px' }}
     />
   );
 };
+
+class Toolbar extends React.Component<ToolbarProps<CalendarEvent, CalendarResource>> {
+  render() {
+    const { label } = this.props;
+    return (
+      <div className="rbc-toolbar">
+        <div className="rbc-toolbar-label">{label}</div>
+      </div>
+    );
+  }
+}
+
+class EventComponent extends React.Component<EventProps<Event>> {
+  render() {
+    const { title, event } = this.props;
+    return (
+      <div className="rbc-event-content">
+        <span style={{ backgroundColor: event.resource, padding: '.2vh 4vh' }}></span>
+        <h5>{title}</h5>
+      </div>
+    );
+  }
+}
 
 export default DnDCalendar;
--- client/src/components/Calendar/MockEvent.ts
+++ client/src/components/Calendar/MockEvent.ts
@@ -2,38 +2,38 @@ export interface IUserSchedule {
   title: string;
   start: Date;
   end: Date;
-  resources: 'InComplete' | 'Completed';
+  resource?: string;
 }
 
 const mockData1: IUserSchedule = {
   title: 'Math',
   start: new Date(2021, 7, 1),
   end: new Date(2021, 7, 1),
-  resources: 'Completed',
+  resource: 'red',
 };
 const mockData2: IUserSchedule = {
   title: 'Science',
   start: new Date(2021, 7, 7),
   end: new Date(2021, 7, 7),
-  resources: 'InComplete',
+  resource: 'blue',
 };
 const mockData3: IUserSchedule = {
   title: 'Social',
   start: new Date(2021, 7, 15),
   end: new Date(2021, 7, 15),
-  resources: 'Completed',
+  resource: 'orange',
 };
 const mockData4: IUserSchedule = {
   title: 'Computer',
   start: new Date(2021, 7, 20),
   end: new Date(2021, 7, 20),
-  resources: 'InComplete',
+  resource: 'white',
 };
 const mockData5: IUserSchedule = {
   title: 'Math Exam',
   start: new Date(2021, 7, 16),
   end: new Date(2021, 7, 16),
-  resources: 'InComplete',
+  resource: 'black',
 };
 
 const mockDatas: IUserSchedule[] = [mockData1, mockData2, mockData3, mockData4, mockData5];
