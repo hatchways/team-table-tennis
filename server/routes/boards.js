c56b864d0875792efbe363a36e689592925137b6
Peter Szadurski <44846197+PeterSzadurski@users.noreply.github.com>
2021-08-10 02:58:10 -0400

    Fixed up node errors (not sure if I should gitignore)

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -37,8 +37,8 @@ router.route('/cards').post(createCard, validateCreateCard);
 router.route('/cards/move').put(moveCard, validateMoveCard);
 
 // Card Detail
-router.route('/cards/getDetail/:cardId').get(protect,getDetails);
-router.route('/cards/detail').put(protect,createDetails);
-router.route('/cards/updateDetails').put(protect,updateDetails);
+//router.route('/cards/getDetail/:cardId').get(protect,getDetails);
+//router.route('/cards/detail').put(protect,createDetails);
+//router.route('/cards/updateDetails').put(protect,updateDetails);
 
 module.exports = router;
