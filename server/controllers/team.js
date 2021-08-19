f5c906a7649220bdf1223819fc054322f20224a8
Alex Yoon <43189680+alexyoon97@users.noreply.github.com>
2021-08-19 12:00:55 -0700

    team.js

--- server/controllers/team.js
+++ server/controllers/team.js
@@ -1,30 +1,30 @@
 const asyncHandler = require("express-async-handler");
 
 
-exports.getTeam = asyncHandler(async (req, res, next) => {
-    const teamId = req.params.teamId
+exports.getTeam = asyncHandler(asnyc (req, res, next) => {
+
 })
 
-exports.deleteTeam = asyncHandler(async (req, res, next) => {
+exports.deleteTeam = asyncHandler(asnyc (req, res, next) => {
     
 })
 
-exports.teamCardAdd = asyncHandler(async (req, res, next) => {
+exports.teamCardAdd = asyncHandler(asnyc (req, res, next) => {
     
 })
 
-exports.teamCardDelete = asyncHandler(async (req, res, next) => {
+exports.teamCardDelete = asyncHandler(asnyc (req, res, next) => {
     
 })
 
-exports.teamCardUpdate = asyncHandler(async (req, res, next) => {
+exports.teamCardUpdate = asyncHandler(asnyc (req, res, next) => {
     
 })
 
-exports.teamBoardAdd = asyncHandler(async (req, res, next) => {
+exports.teamBoardAdd = asyncHandler(asnyc (req, res, next) => {
     
 })
 
-exports.teamBoardDelete = asyncHandler(async (req, res, next) => {
+exports.teamBoardDelete = asyncHandler(asnyc (req, res, next) => {
     
 })
