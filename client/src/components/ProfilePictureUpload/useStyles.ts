8701f810268d3f88f5378084fa72445cbd8d9770
hamidrc <77899847+hamidrc@users.noreply.github.com>
2021-08-19 12:58:44 -0400

    Updated useStyles.tsx
    Created a more responsive skin

--- client/src/components/ProfilePictureUpload/useStyles.ts
+++ client/src/components/ProfilePictureUpload/useStyles.ts
@@ -12,7 +12,10 @@ const useStyles = makeStyles((theme: Theme) => ({
   uploadTitle: {
     fontSize: '1.5em',
     fontWeight: 600,
-    padding: '80px 0px 5px 0px',
+    padding: '10% 0px 0% 0px',
+    [theme.breakpoints.down('sm')]: {
+      padding: '10% 0px 0% 0px',
+    },
   },
   chooseimageButton: {
     width: '12vw',
@@ -20,27 +23,67 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: 'flex',
     flexWrap: 'nowrap',
     padding: '0px 0px 0px 0px',
+    border: '2px dashed #000',
     background: 'linear-gradient(45deg, rgba(117,156,252,255) 30%, #64b5f6 90%)',
     borderRadius: 6,
     boxShadow: 'hidden',
-    color: 'white',
+    color: 'black',
     '&:hover': {
       background: '#2962ff',
       color: 'white',
     },
+    [theme.breakpoints.down('sm')]: {
+      width: '15vw',
+      height: '7vh',
+      display: 'flex',
+      flexWrap: 'nowrap',
+      padding: '0px 0px 0px 0px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5vh',
+      width: '19vw',
+      padding: '0% 0px 0% 0%',
+    },
   },
   submitButton: {
-    padding: '20% 0px 20% 4vw',
+    height: '4vh',
+    width: '10vw',
+    padding: '20% 0px 30% 30%',
+    [theme.breakpoints.down('sm')]: {
+      height: '5%',
+      width: '10%',
+      padding: '5% 0px 5% 8%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5%',
+      width: '10%',
+      padding: '5% 0px 5% 0%',
+    },
   },
   profileImage: {
-    width: '55%',
+    width: '100%',
     height: 'undefined',
     aspectRatio: '1',
     borderRadius: '50%',
   },
   imageBox: {
-    height: '200px',
-    padding: '20px 0px 0px 0px',
+    height: '50%',
+    width: '10vw',
+    padding: '10% 0px 0px 10%',
+    [theme.breakpoints.down('sm')]: {
+      width: '40%',
+      height: 'undefined',
+      padding: '10% 5% 0px 0px',
+      aspectRatio: '1',
+      borderRadius: '50%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '60%',
+      height: 'undefined',
+      padding: '10% 5% 0px 0px',
+      aspectRatio: '1',
+      borderRadius: '50%',
+    },
   },
 }));
 
